db2bb64ff9c7a6d7afc7b6817d16a3c72d727738
rlane728 <rlane728@gmail.com>
2023-09-16 08:05:38 -0500

    Day 2 start

---
+++ Day02/main_day02.py
@@ -0,0 +1,8 @@
+# len() can be used to return the length of a string
+print(len("Hello"))
+
+# But will fail to return the length ogf int
+# print(len(12345)) produces an error
+
+# Strings themselves can be indexed
+print("Robert"[2])
