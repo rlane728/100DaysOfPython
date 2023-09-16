f4e75cb2006da9c6315803c583748184b71f6984
rlane728 <rlane728@gmail.com>
2023-09-16 08:25:31 -0500

    Day 2 contd

--- Day02/main_day02.py
+++ Day02/main_day02.py
@@ -5,4 +5,20 @@ print(len("Hello"))
 # print(len(12345)) produces an error
 
 # Strings themselves can be indexed
+# This is called "subscripting"
 print("Robert"[2])
+
+# Large integers use underscores instead of comma separators
+# print(int(100,000,000)) ... This throws an error
+print(int(100_000_000))
+
+# Strings can be cast to int and vice versa
+string_a = "123"
+int_b = 456
+swap_c = int(string_a)
+string_a = str(int_b)
+int_b = swap_c
+print(string_a)
+print(int_b)
+print(type(string_a))
+print(type(int_b))
