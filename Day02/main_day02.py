b25ef44317ae70426cb41417c332fbb79dd9f8e0
rlane728 <rlane728@gmail.com>
2023-09-16 10:51:36 -0500

    Day02 updates

--- Day02/main_day02.py
+++ Day02/main_day02.py
@@ -1,3 +1,21 @@
+# This is a string
+string_hello = "Hello"
+print(type(string_hello))
+
+# This is an int
+int_123 = 123
+print(type(int_123))
+
+# This is a float
+float_12567 = 12.567
+print(type(float_12567))
+
+# This is a bool
+bool_true = True
+bool_false = False
+print(type(bool_true))
+print(type(bool_false))
+
 # len() can be used to return the length of a string
 print(len("Hello"))
 
@@ -13,6 +31,7 @@ print("Robert"[2])
 print(int(100_000_000))
 
 # Strings can be cast to int and vice versa
+# I'm sure this can be done with the others, too
 string_a = "123"
 int_b = 456
 swap_c = int(string_a)
@@ -22,3 +41,20 @@ print(string_a)
 print(int_b)
 print(type(string_a))
 print(type(int_b))
+
+# A print statement by default can only concatenate equivalent data types
+string_print1 = "Hello"
+int_print1 = 12345
+print (string_print1 + " User!")
+
+# The following will throw an error because the data types differ
+# print (int_print1 + string_print1)
+
+# ints and floats can be combined
+int_float = 100
+float_int = 23.3
+print(int_float + float_int)
+intf_out = print(int_float + float_int)
+print(type(intf_out))
+intf_plus = int_float + float_int
+print(type(intf_plus))
---
+++ Day02/vartest.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+addend_a = int(two_digit_number[0])
+addend_b = int(two_digit_number[1])
+my_sum = str(addend_a + addend_b)
+print(two_digit_number[0] + " + " + two_digit_number[1] + " = " + my_sum)
